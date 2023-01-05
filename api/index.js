31c14b90a031ae484afa66ba701213e0980be786
Jaron <jaronchretien@yahoo.com>
2023-01-05 15:14:58 -0600

    feat(TokenVerification): Verfies headers token

--- api/index.js
+++ api/index.js
@@ -1,31 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { getUserByUsername } = require("../db/users");
 const { JWT_SECRET } = process.env;
 
+// router.use(async (req, res, next) => {
+//   try {
+//     const auth = req.header("Authorization");
+//     if (!auth) {
+//       next();
+//     } else {
+//       const [_, token] = auth.split(' ');
+//       console.log(token)
+//       const { id, username } = jwt.verify(token, JWT_SECRET);
+//       console.log(username)
+//       if (!username) {
+//         next({
+//           error: "InvalidToken",
+//           name: "TokenError",
+//           message: "Token is invalid",
+//         });
+//       } else {
+//         req.user = await getUserByUsername(username);
+//         next();
+//       }
+//     }
+//   }catch (error) {
+//     throw error;
+//   }
+// });
+
 router.use(async (req, res, next) => {
-  try {
-    const auth = req.header("Authorization");
-    if (!auth) {
-      next();
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+
+    if(!auth) {
+        next ()
+    }else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length)
+        try {
+            const {id, username} = jwt.verify(token, JWT_SECRET)
+            if (username) {
+                req.user = await getUserByUsername(username)
+                next ()
+            }
+        }catch({name, message}) {
+            next({name, message })
+        }
     } else {
-      const [_, token] = auth.split();
-      const { id, username } = jwt.verify(token, JWT_SECRET);
-      if (!username) {
-        next({
-          error: "InvalidToken",
-          name: "TokenError",
-          message: "Token is invalid",
-        });
-      } else {
-        req.user = await getUserByUsername(username);
-        next();
-      }
+        next ({
+            name: 'AuthoizationHeaderError',
+            message: `Authorization token must start with ${ prefix }`
+        })
     }
-  } catch (error) {
-    throw error;
-  }
 });
+
 
 const userRouter = require("./users");
 router.use("/users", userRouter);
