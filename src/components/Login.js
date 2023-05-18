537a625f46d3416ae32e9ff3f058b932f1f33282
Jaron <jaronchretien@yahoo.com>
2023-05-18 15:34:54 -0500

    feat(register) Completes register and login as well as edgecases;

--- src/components/Login.js
+++ src/components/Login.js
@@ -52,16 +52,17 @@ const Login = ({setToken}) => {
     const handleRegister = async (event) => {
         event.preventDefault()
         if(password !== password2) {
-            setErrorMessage('Paswords do not match!')
-        }
-        const response = await registerUser({username : username, password: password})
-        if (response.error) {
-            setErrorMessage(response.message)
+            setErrorMessage('Paswords do not match')
         }else {
-            setErrorMessage('')
-            setToken(response.token)
-            localStorage.setItem('token', response.token)
-            navigate('/')
+            const response = await registerUser({username : username, password: password})
+            if (response.error) {
+                setErrorMessage(response.message)
+            }else {
+                setErrorMessage('')
+                setToken(response.token)
+                localStorage.setItem('token', response.token)
+                navigate('/')
+            }
         }
        
     }
@@ -70,33 +71,35 @@ const Login = ({setToken}) => {
     return (
         <div className='login-page'>
                 <div className='loginFormContainer'>
-                    <form className='loginForm'>
-                    <h1 className='welcomeTitle'>Welcome Back</h1>
+                    <form onSubmit={(currentAction == 'register' ? handleRegister : handleLogin)}className='loginForm'>
+                    <h1 className='welcomeTitle'>Welcome {currentAction == 'register' ? '' : 'Back'}</h1>
                         <div className='inputBox'>
                             <div className='iconBox'>
                                 <img className='loginImage' src='/images/icons8-person-60.png'/>
-                                <input placeholder='Username...'></input>
+                                <input value={username} onChange={(e) => setUsername(e.target.value)}  placeholder='Username...'></input>
                             </div>
                         </div>
                         <div className='inputBox'>
                             <div className='iconBox'>
                                 <img className='loginImage' src='/images/icons8-lock-50.png'/>
-                                <input placeholder='Password...'></input>
+                                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password...'></input>
                             </div>
                         </div>
-                        {currentAction !== 'register'? null : 
-                        <div className='inputBox'>
+                 
+                        <div style={{display: (currentAction == 'register' ? 'block' : 'none')}}className='inputBox'>
                             <div className='iconBox'>
                                 <img className='loginImage' src='/images/icons8-lock-50.png'/>
-                                <input placeholder='Confirm Password...'></input>
+                                <input type='password' value={password2} onChange={(e) => setPassword2(e.target.value)} placeholder='Confirm Password...'></input>
                             </div>
                         </div>
-                        }
+                        
                         <button className='loginButton'>Login</button>
+                        <span style={{scale: (errorMessage ? '100%': '0')}}className='errorMessage'>{errorMessage}</span>
                         <h3>{currentAction == 'register' ? 
-                        <span>Have an account? <span onClick={() => setCurrentAction('login')}className='linkSpan'>Login</span></span>
+                        <span>Have an account? <span onClick={() => {setCurrentAction('login'), setErrorMessage(''), setPassword2(''), setUsername(''), setPassword('')}}className='linkSpan'>Login</span></span>
                         : 
-                        <span>Don't have an account? <span onClick={() => setCurrentAction('register')} className='linkSpan' >Sign up</span></span>}</h3>
+                        <span>Don't have an account? <span onClick={() => {setCurrentAction('register'), setErrorMessage(''), setPassword2(''), setUsername(''), setPassword('')}} className='linkSpan' >Sign up</span></span>}</h3>
+                        
                     </form >
                 </div>
         </div>
