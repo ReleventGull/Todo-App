4ef57fcc96b2cf349f4fe419c169a8b9c9098cc4
Jaron <jaronchretien@yahoo.com>
2023-05-20 12:34:49 -0500

    feat(navbar) Reworks the navbar to look better and be responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,26 +7,35 @@ import {Link, Outlet} from 'react-router-dom'
 const NavBar = () => {
     return (
         <>
-            <div className='sideNav'>
-            <nav>
-            <Link to='/'>
-                <img className='logoutButton' src={home}/> 
-                <span className='wordReveal'>Home</span>
-            </Link>
-            <Link to='todos'>
-                <img className='logoutButton' src={note}/> 
-                <span className='wordReveal'>Tasks</span>
-            </Link>
-            <Link to='createTodo'>
-                <img className='logoutButton' src={plus}/> 
-                <span className='wordReveal'>Create</span>
-            </Link>
+           
+            <nav className='navbar'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <Link to='/'>
+                            <img className='logoutButton' src={home}/> 
+                            <span className='wordReveal'>Home</span>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='todos'>
+                            <img className='logoutButton' src={note}/> 
+                            <span className='wordReveal'>Tasks</span>
+                        </Link>
+                    </li>  
+                    <li className='nav-item'>
+                        <Link to='createTodo'>
+                            <img className='logoutButton' src={plus}/> 
+                            <span className='wordReveal'>Create</span>
+                        </Link>
+                    </li>    
+                    <li className='nav-item'>
+                        <Link  to='/login' onClick={() => {setToken(''), localStorage.removeItem('token')}}> 
+                            <img className='logoutButton' src={logout}/>
+                            <span className='wordReveal'>Logout</span>
+                        </Link>
+                    </li>     
+                </ul>
             </nav>
-            <Link  to='/login' onClick={() => {setToken(''), localStorage.removeItem('token')}}> 
-                <img className='logoutButton' src={logout}/>
-                <span className='logoutWord'>Logout</span>
-            </Link>
-            </div>
             <Outlet />
         </>
             
